0ef85dc08ab4c1710eb4c7caca59d3ec71a9f038
ChinoBeach <beach988@morrisville.edu>
2022-02-15 20:16:54 -0500

    How to Destroy Objects

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    //variable time delay when destorying an object
+    [SerializeField] float fltDestoryDelay = 0.5f;
+    //variable to see if we have picked up the package
+    bool bolHasPackage;
+
     //When two things collide
     void OnCollisionEnter2D(Collision2D other) 
     {
@@ -12,16 +17,24 @@ public class Delivery : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        //IF the trigger is the package, print "package aquired" to he console
-        if(other.tag == "Package")
+        //IF the trigger is the package
+        if(other.tag == "Package" && !bolHasPackage)
         {
+            //note that we have the package
+            bolHasPackage = true;
             Debug.Log("Package Aquired");
+
+            //remove the package from the game 
+            Destroy(other.gameObject, fltDestoryDelay);
         }
 
-        //IF the trigger is a cusomer, print "package delivered" to the console
-        if(other.tag == "Customer")
+        //IF the trigger is a cusomer,AND we have the package
+        if(other.tag == "Customer" && bolHasPackage == true)
         {
+            //note that we no longer have the package
+            bolHasPackage = false;
             Debug.Log("Package Delivered");
+
         }
 
     }
