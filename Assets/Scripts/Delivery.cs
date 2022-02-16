f53b9f6e680787a1766ac631f2c6dd3f8ec1a83e
ChinoBeach <beach988@morrisville.edu>
2022-02-15 20:32:15 -0500

    How to use GetCompnent

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -4,10 +4,27 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
-    //variable time delay when destorying an object
-    [SerializeField] float fltDestoryDelay = 0.5f;
+    //variables car colors
+    [SerializeField] Color32 hasPackageColor = new Color32(1, 1, 1, 1);
+    [SerializeField] Color32 noPackageColor = new Color32(1, 1, 1, 1);
+    
+    //Variable for changing car color
+    SpriteRenderer spriteRenderer;
+    
+    
     //variable to see if we have picked up the package
     bool bolHasPackage;
+
+    //variable time delay when destorying an object
+    [SerializeField] float fltDestoryDelay = 0.5f;
+
+    
+
+    
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     //When two things collide
     void OnCollisionEnter2D(Collision2D other) 
@@ -24,6 +41,9 @@ public class Delivery : MonoBehaviour
             bolHasPackage = true;
             Debug.Log("Package Aquired");
 
+            //change car color
+            spriteRenderer.color = hasPackageColor;
+
             //remove the package from the game 
             Destroy(other.gameObject, fltDestoryDelay);
         }
@@ -34,6 +54,9 @@ public class Delivery : MonoBehaviour
             //note that we no longer have the package
             bolHasPackage = false;
             Debug.Log("Package Delivered");
+
+            //change car color
+            spriteRenderer.color = noPackageColor;
 
         }
 
